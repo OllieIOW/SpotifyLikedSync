351792bc03d490dab4fa5b1791042c5096e33b74
OllieIOW <95285960+OllieIOW@users.noreply.github.com>
2024-08-09 00:44:43 +0100

    Update sync.py
    Added feature that removes any songs you have un-liked

--- sync.py
+++ sync.py
@@ -37,6 +37,22 @@ if not playlist_id:
     playlist = sp.user_playlist_create(user_id, playlist_name)
     playlist_id = playlist['id']
 
+playlist_tracks = []
+results = sp.playlist_tracks(playlist_id)
+while results:
+    for item in results['items']:
+        track = item['track']
+        playlist_tracks.append(track['id'])
+    if results['next']:
+        results = sp.next(results)
+    else:
+        results = None
+
+songs_to_remove = [track_id for track_id in playlist_tracks if track_id not in liked_songs]
+
+if songs_to_remove:
+    sp.user_playlist_remove_all_occurrences_of_tracks(user_id, playlist_id, songs_to_remove)
+
 sp.user_playlist_replace_tracks(user_id, playlist_id, liked_songs[:100])
 
 for i in range(100, len(liked_songs), 100):
@@ -47,4 +63,10 @@ description = f"This playlist was synced automatically at {sync_time}"
 sp.user_playlist_change_details(user_id, playlist_id, description=description)
 
 print(f'Synced {len(liked_songs)} liked songs to the playlist "{playlist_name}".')
+
+if songs_to_remove:
+    print(f'Removed {len(songs_to_remove)} un-liked songs from the playlist.')
+else:
+    print(f'No un-liked songs to remove.')
+
 print(f'Playlist description updated to: "{description}"')
